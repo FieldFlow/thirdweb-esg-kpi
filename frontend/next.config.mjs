22d032f6020235a51736cb943f41f4e48b9fe8ee
Egor Ermolaev <egorostu@gmail.com>
2024-11-10 17:20:33 +0100

    fix(redirection): both domains

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,6 +5,18 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://thirdweb-esg-kpi.vercel.app/:path*',
+      },
+      {
+        source: '/:path*',
+        destination: 'https://esg.fieldflow.lu/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
